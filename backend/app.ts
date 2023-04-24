8e9439d9f13216281b04a2bef9f4c4b903c03c17
terryhycheng <terryhycheng@gmail.com>
2023-04-24 12:18:04 +0100

    Added `server.ts` and exported `App` from `app.ts`
    Co-authored-by: Amber <118190812+AmberG31@users.noreply.github.com>

--- backend/app.ts
+++ backend/app.ts
@@ -2,17 +2,9 @@ import express from "express";
 import usersRouter from "./routes/users";
 import cors from "cors";
 const app = express();
-const port = 8081;
 
 app.use(cors());
 
-// http://localhost:8081/
-app.get("/", (req, res) => {
-  res.send({ message: "Hello World!" });
-});
-
 app.use("/users", usersRouter);
 
-app.listen(port, () => {
-  console.log(`Express server is now running at http://localhost:${port}`);
-});
+export default app;
