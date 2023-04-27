67b3093898d584dd491db0a9120733841f02c94b
AmberG31 <gynteee@yahoo.co.uk>
2023-04-27 12:54:34 +0100

    Connected back end
    Co-authored-by: terryhycheng <terryhycheng@gmail.com>

--- frontend/src/components/TaskList/TaskList.tsx
+++ frontend/src/components/TaskList/TaskList.tsx
@@ -1,37 +1,26 @@
 import Task from "./Task";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 import { type Task as TaskType } from "../../../../types/task";
 import styles from "./TaskList.module.scss";
 
-const fakeTasks: TaskType[] = [
-  {
-    id: 1,
-    title: "sweep the floor",
-    content: "have to do it before your mum comes back",
-    due_date: "2023-04-26",
-    is_completed: true,
-    is_priority: false,
-    createdAt: "2023-04-26T10:16:46.121Z",
-  },
-  {
-    id: 2,
-    title: "drink water",
-    content: "8 glasses of water per day",
-    due_date: "2023-02-24",
-    is_completed: false,
-    is_priority: true,
-    createdAt: "2023-04-26T10:16:46.121Z",
-  },
-];
-
 const TaskList = () => {
+  const fetchData = async (): Promise<TaskType[]> => {
+    const res = await axios.get("http://localhost:8081/tasks/1");
+    return res.data;
+  };
+
+  const { data, status } = useQuery(["tasks"], fetchData);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>My Tasks</h1>
       <div className={styles.list}>
-        {fakeTasks.map((task) => (
-          <Task key={task.id} task={task} />
-        ))}
+        {status === "success" &&
+          data.map((task) => <Task key={task.id} task={task} />)}
       </div>
+      {status === "loading" && <p>Loading...</p>}
+      {status === "error" && <p>Something went wrong</p>}
     </div>
   );
 };
